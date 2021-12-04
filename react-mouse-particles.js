dc9c77ff977709675a70ef0bb1b10a94faf987a1
Asif Mohammed Sifat <86705300+AsifMohammedSifat@users.noreply.github.com>
2021-12-04 14:43:08 -0600

    Create react-mouse-particles.js
    If you move your mouse you will see magic...
    #react-mouse-particles

---
+++ react-mouse-particles.js
@@ -0,0 +1,22 @@
+This is an mouse move animation.
+needed link here=>
+                    particle_bg: https://www.npmjs.com/package/particles-bg +scroll down
+                    package github document link: https://github.com/lindelof/react-mouse-particles
+
+step:1=>install package
+
+        npm i react-mouse-particles
+        
+        
+step:3=>drop that code in -> app.js file
+
+
+        <MouseParticles
+          g={1}
+          color="random"
+          cull="MuiSvgIcon-root,MuiButton-root"
+          level={6}
+        />
+            
+step:4=> import MouseParticles from "react-mouse-particles";
+
