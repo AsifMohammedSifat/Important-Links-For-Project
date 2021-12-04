576d22e83d0eb121e5bafaacd14a1402a69ce143
Asif Mohammed Sifat <86705300+AsifMohammedSifat@users.noreply.github.com>
2021-12-04 15:29:06 -0600

    Create typing-particles-power-mode-input.js

---
+++ typing-particles-power-mode-input.js
@@ -0,0 +1,8 @@
+If you type you will see magic
+
+Needed link=>
+                https://github.com/lindelof/power-mode-input
+
+ step:1=>install packege
+                  npm install --save power-mode-input
+step:2=> ########not-completed####
